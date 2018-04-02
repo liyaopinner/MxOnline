633d8026ba70505f88284b8bebbc11b822831b05
liyaopinner <liyao.pinner@gmail.com>
2018-04-02 17:21:57 +0800

    修复验证码一直提示验证码失败的bug
    修复验证码一直提示验证码失败的bug

--- MxOnlie/static/js/login.js
+++ MxOnlie/static/js/login.js
@@ -45,7 +45,7 @@ function zy_Countdown(){
 function refresh_captcha(event){
     $.get("/captcha/refresh/?"+Math.random(), function(result){
         $('#'+event.data.form_id+' .captcha').attr("src",result.image_url);
-        $('#'+event.data.form_id+' .form-control-captcha[type="hidden"]').attr("value",result.key);
+        $('#id_captcha_0').attr("value",result.key);
     });
     return false;
 }
