e1ddc1f2697a01d78d0016604ea239e38e83c364
liyaopinner <liyao.pinner@gmail.com>
2018-05-10 16:06:47 +0800

    修复excel上传出现403的bug

--- MxOnlie/extra_apps/xadmin/plugins/excel.py
+++ MxOnlie/extra_apps/xadmin/plugins/excel.py
@@ -3,6 +3,7 @@
 import xadmin
 from xadmin.views import BaseAdminPlugin, ListAdminView
 from django.template import loader
+from xadmin.plugins.utils import get_context_dict
 
 
 #excel 导入
@@ -13,7 +14,7 @@ class ListImportExcelPlugin(BaseAdminPlugin):
         return bool(self.import_excel)
 
     def block_top_toolbar(self, context, nodes):
-        nodes.append(loader.render_to_string('xadmin/excel/model_list.top_toolbar.import.html', context={}))
+        nodes.append(loader.render_to_string('xadmin/excel/model_list.top_toolbar.import.html', context=get_context_dict(context)))
 
 
 xadmin.site.register_plugin(ListImportExcelPlugin, ListAdminView)
