b7f8a497310f0cdaa0e7388459299b0f448b4036
liyaopinner <liyao.pinner@gmail.com>
2018-06-02 11:37:08 +0800

    修复inline加号出现的bug

--- MxOnlie/extra_apps/xadmin/templatetags/xadmin_tags.py
+++ MxOnlie/extra_apps/xadmin/templatetags/xadmin_tags.py
@@ -1,10 +1,13 @@
 from django import template
 from django.template import Library
+from django.utils import six
 from django.utils.safestring import mark_safe
+from django.utils.html import escape
 
 from xadmin.util import static, vendor as util_vendor
 
 register = Library()
+
 
 @register.simple_tag(takes_context=True)
 def view_block(context, block_name, *args, **kwargs):
@@ -15,20 +18,23 @@ def view_block(context, block_name, *args, **kwargs):
     nodes = []
     method_name = 'block_%s' % block_name
 
+    cls_str = str if six.PY3 else basestring
     for view in [admin_view] + admin_view.plugins:
         if hasattr(view, method_name) and callable(getattr(view, method_name)):
             block_func = getattr(view, method_name)
             result = block_func(context, nodes, *args, **kwargs)
-            if result and type(result) in (str, unicode):
+            if result and isinstance(result, cls_str):
                 nodes.append(result)
     if nodes:
         return mark_safe(''.join(nodes))
     else:
         return ""
 
+
 @register.filter
 def admin_urlname(value, arg):
     return 'xadmin:%s_%s_%s' % (value.app_label, value.model_name, arg)
+
 
 static = register.simple_tag(static)
 
@@ -40,13 +46,14 @@ def vendor(context, *tags):
 
 class BlockcaptureNode(template.Node):
     """https://chriskief.com/2013/11/06/conditional-output-of-a-django-block/"""
+
     def __init__(self, nodelist, varname):
         self.nodelist = nodelist
         self.varname = varname
 
     def render(self, context):
         output = self.nodelist.render(context)
-        context[self.varname] = str(output)
+        context[self.varname] = escape(output)
         return ''
 
 
